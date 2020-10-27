675ce47e1cfb110341febb5244cf5224f490a93e
Jacky <jackypan301@gmail.com>
2020-10-27 16:04:16 -0700

    finsihed readall

--- datastore/index.js
+++ datastore/index.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
 const counter = require('./counter');
+const Promise = require('bluebird');
+const readFilePromise = Promise.promisify(fs.readFile);
 
 var items = {};
 
 // Public API - Fix these CRUD functions ///////////////////////////////////////
-
+//dadadads
 //take this js obj in memory and write it to a file - json.stringify
 
 exports.create = (text, callback) => {
@@ -28,45 +30,78 @@ exports.create = (text, callback) => {
 };
 //use JSON.parse to read the file on hardrive and have it render back on to the client?????
 
+
+
+
 exports.readAll = (callback) => {
 
-  fs.readdir(exports.dataDir, (err, file) => {
+  fs.readdir(exports.dataDir, (err, files) => {
     if (err) {
       console.log('error thrown');
       throw ('error');
     } else {
       // console.log(file);
-      if (file.length === 0) {
+      if (files.length === 0) {
         callback(null, []);
       } else {
 
-        var data = [];
+        var data = _.map(files, (file) => {
+          console.log(file);
+          return readFilePromise(`${exports.dataDir}/${file}`).then((fileData) => {
+            console.log(fileData.toString());
+            return {
+              'id': file.substring(0, file.length - 4), 'text': fileData.toString()
+            };
+          });
+        });
+        Promise.all(data)
+          .then((items) =>
+            callback(null, items),
+          err => callback(err));
+
+
         // // var createArray = (data, (err, data) => {
         // //   if (err) {
         // //     throw err;
         // //   } else {
-        _.map(file, (id, text) => {
-          fs.readFile(`${exports.dataDir}/${id}`, (err, result) => {
-            if (err) {
-              callback(err);
-              console.log('error thrown line 51');
-            } else {
-              console.log(result.toString());
-              data.push( {'id': id.substring(0, id.length - 4), 'text': result.toString()});
-              console.log(file);
-              // callback(null, data);
-            }
-          });
-        });
-        console.log(data);
-        // callback(null, data);
+        // _.map(file, (id, text) => {
+        //   fs.readFile(`${exports.dataDir}/${id}`, (err, result) => {
+        //     if (err) {
+        //       callback(err);
+        //       console.log('error thrown line 51');
+        //     } else {
+        //       console.log(result.toString());
+        //       data.push( {'id': id.substring(0, id.length - 4), 'text': result.toString()});
+        //       console.log(file);
+        //       // callback(null, data);
+        //     }
+        //   });
+        // });
+        // console.log(data);
+        // // callback(null, data);
         // }
       }
-      // return { id, text };
-      // });
     }
   });
+
 };
+
+exports.mappedDir = function (files, data) {
+  return new Promise(function (resolve, reject) {
+    _.map(files, (id, text) => {
+      fs.readFile(`${exports.dataDir}/${id}`, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          data.push({ 'id': id.substring(0, id.length - 4), 'text': result.toString() });
+          // callback(null, data);
+        }
+      });
+    });
+  });
+};
+
+
 
 exports.readOne = (id, callback) => {
   fs.readFile(`${exports.dataDir}/${id}.txt`, (err, result) => {
@@ -79,15 +114,15 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  fs.readFile (`${exports.dataDir}/${id}.txt`, (err) => {
+  fs.readFile(`${exports.dataDir}/${id}.txt`, (err) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
     } else {
-      fs.writeFile ( `${exports.dataDir}/${id}.txt`, text, (err) => {
+      fs.writeFile(`${exports.dataDir}/${id}.txt`, text, (err) => {
         if (err) {
           callback(new Error(`No item with id: ${id}`));
         } else {
-          callback(null, { 'id': id, 'text': text});
+          callback(null, { 'id': id, 'text': text });
         }
       });
     }
@@ -103,7 +138,7 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  fs.readFile (`${exports.dataDir}/${id}.txt`, (err) => {
+  fs.readFile(`${exports.dataDir}/${id}.txt`, (err) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
     } else {
